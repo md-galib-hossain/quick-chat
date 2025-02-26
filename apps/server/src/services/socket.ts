191175f040cf271457e938b2a2279c1ebea7c65b
Md Galib Hossain <mdgalib23@gmail.com>
2025-02-26 23:51:32 +0600

    feat:socket event listeners

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -5,8 +5,19 @@ class SocketService {
     console.log("Init Socket Server...");
     this._io = new Server();
   }
-  get io(){
+  public initListeners() {
+    console.log(`Initialized socket listeners`)
+    const io = this.io;
+    io.on("connect", (socket) => {
+      console.log(`New Socket Connected`, socket.id);
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log(`New Message Recieved`, message);
+      });
+    });
+  }
+
+  get io() {
     return this._io;
   }
 }
-export default SocketService
+export default SocketService;
